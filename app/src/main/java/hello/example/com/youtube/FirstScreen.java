36f773eb603ed7423fcd3d9f616654957f805d0a
Parth <parth1493@gmail.com>
2017-05-19 21:52:56 -0500

    Branch Try Parth

--- app/src/main/java/hello/example/com/youtube/FirstScreen.java
+++ app/src/main/java/hello/example/com/youtube/FirstScreen.java
@@ -163,7 +163,7 @@ public class FirstScreen extends AppCompatActivity {
                     }
                 }
             }
-
+            //Created my branch.
             // other 'case' lines to check for other
             // permissions this app might request
         }
