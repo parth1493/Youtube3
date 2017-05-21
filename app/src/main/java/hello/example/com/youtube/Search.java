bad5dfc7aae2e402de70b41bd14002de0fe9fbef
Parth <parth1493@gmail.com>
2017-05-20 23:24:53 -0400

    p7

--- app/src/main/java/hello/example/com/youtube/Search.java
+++ app/src/main/java/hello/example/com/youtube/Search.java
@@ -2,12 +2,30 @@ package hello.example.com.youtube;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+
+import java.util.ArrayList;
 
 public class Search extends AppCompatActivity {
+    String[] languages={"Android ","java","IOS","SQL","JDBC","Web services,HGG,hGD,Rt,HGu,HGJ"};
+    AutoCompleteTextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
+        ArrayList<String> arrayOfUsers = new ArrayList<String>();
+       arrayOfUsers.add("java");
+        arrayOfUsers.add("c++");
+        arrayOfUsers.add("javaqds");
+        arrayOfUsers.add("javaasdfas");
+
+        text=(AutoCompleteTextView)findViewById(R.id.autoText);
+        UsersAdapter adapter = new UsersAdapter(this, arrayOfUsers);
+
+        text.setAdapter(adapter);
+        text.setThreshold(1);
+
     }
 }
---
+++ app/src/main/java/hello/example/com/youtube/User.java
@@ -0,0 +1,14 @@
+package hello.example.com.youtube;
+
+/**
+ * Created by krishna on 2017-05-20.
+ */
+
+public class User {
+    public String name;
+
+
+    public User(String name) {
+        this.name = name;
+    }
+}
